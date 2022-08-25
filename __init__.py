8f1f236e82e302c5608a8ed463b5e656c847ed4f
ljiredale <ljiredale@student.nafcs.org>
2022-08-24 22:51:47 -0400

    added things

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from flask import *
+import os
 
 
 app = Flask(__name__)
@@ -7,5 +8,8 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route("/2016whitefordfusion")
+def whitefordfusion():
+    return render_template('2016whitefordfusion.html')
 
 app.run(debug=True)
